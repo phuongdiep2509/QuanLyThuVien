5bea7b07a56d11bffa54d282cc4ccfcb22d01e8e
Phamtrang940 <26a4042153@hvnh.edu.vn>
2026-05-31 10:31:14 +0700

    BLL Dashboard và tìm kiếm

---
+++ MuonTraSach/BLL/DashboardBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using Dashboard_Search.DAL;
+using Dashboard_Search.DTO;
+
+namespace Dashboard_Search.BLL
+{
+    public class DashboardBLL
+    {
+        private DashboardDAL dal = new DashboardDAL();
+
+        public DashboardDTO LaySoLieuThongKe(DateTime tuNgay, DateTime denNgay) { return dal.LaySoLieuThongKe(tuNgay, denNgay); }
+        public DataTable LayDanhSachMoi(DateTime tuNgay, DateTime denNgay) { return dal.LayDanhSach("Mới", tuNgay, denNgay); }
+        public DataTable LayDanhSachQuaHan(DateTime tuNgay, DateTime denNgay) { return dal.LayDanhSach("Quá hạn", tuNgay, denNgay); }
+        public DataTable LayDuLieuBieuDo(DateTime tuNgay, DateTime denNgay) { return dal.LayDuLieuBieuDo(tuNgay, denNgay); }
+    }
+}
---
+++ MuonTraSach/BLL/SearchBLL.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using Dashboard_Search.DAL;
+
+namespace Dashboard_Search.BLL
+{
+    public class SearchBLL
+    {
+        private SearchDAL dal = new SearchDAL();
+
+        public DataTable TimKiem(string tuKhoa) { return dal.TimKiemToanHeThong(tuKhoa); }
+        public DataTable LayChiTiet(string phanLoai, string ma) { return dal.LayChiTiet(phanLoai, ma); }
+    }
+}
