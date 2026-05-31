b5c0eff8f80f9997ee474d46eeca7d6212f12539
Phamtrang940 <26a4042153@hvnh.edu.vn>
2026-05-31 10:35:27 +0700

    DTO Dashboard Search

---
+++ MuonTraSach/DTO/DashboardDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard_Search.DTO
+{
+    public class DashboardDTO
+    {
+        public int TongSach { get; set; }
+        public int TongDocGia { get; set; }
+        public int SachDangMuon { get; set; }
+        public int SachQuaHan { get; set; }
+    }
+}
---
+++ MuonTraSach/DTO/SearchResultDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard_Search.DTO
+{
+    public class SearchResultDTO
+    {
+        public string PhanLoai { get; set; } // "Tài liệu", "Tác giả", "Phiếu mượn"
+        public string MaDoiTuong { get; set; } // MaTaiLieu, MaTacGia, MaPhieuMuon
+        public string ThongTinChiTiet { get; set; } // Tên sách, Tên tác giả, v.v.
+    }
+}
